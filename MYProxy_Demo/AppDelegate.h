2792ba14422cc5558e3d6aa12c5168f4b0ad5fa1
B.Ding <742819176@qq.com>
2018-07-19 22:01:19 +0800

    Initial Commit

---
+++ MYProxy_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYProxy_Demo
+//
+//  Created by dingbinbin on 2018/7/19.
+//  Copyright © 2018年 dingbinbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYProxy_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYProxy_Demo
+//
+//  Created by dingbinbin on 2018/7/19.
+//  Copyright © 2018年 dingbinbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
